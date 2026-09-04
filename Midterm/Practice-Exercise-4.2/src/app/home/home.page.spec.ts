eb4b874840e4e4d9e232fe6bf21eb7ee21a24666
sewadotwav <dimalantababymariec@gmail.com>
2026-09-04 19:09:01 +0800

    midtermpe42

---
+++ Midterm/Practice-Exercise-4.2/src/app/home/home.page.spec.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonContent],
+})
+export class HomePage {
+  constructor() {}
+}
